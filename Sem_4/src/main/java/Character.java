dabe7a5feb81f567f09420b729cafbeda4462540
Igor Pudovkin <iggpd62@yandex.ru>
2023-07-31 18:49:40 +0300

    third

--- Sem_4/src/main/java/Character.java
+++ Sem_4/src/main/java/Character.java
@@ -4,14 +4,12 @@ abstract class BaseHero implements CharacterInterface {
     protected int speed;
     protected Coordinates coordinates;
 
-    public BaseHero(String name, int health, int speed, int x, int y) {
+    protected BaseHero(String name, int health, int speed, int x, int y) {
         this.name = name;
         this.health = health;
         this.speed = speed;
         this.coordinates = new Coordinates(x, y);
     }
-
-
     public void attack() {
         System.out.println(name + " наносит удар!");
     }
@@ -32,4 +30,3 @@ abstract class BaseHero implements CharacterInterface {
         return coordinates;
     }
 }
-
